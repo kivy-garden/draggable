9c443b9027e7144886dad8a4b5b40de66aa14f33
gottadiveintopython <flow4re2c@gmail.com>
2021-01-29 11:39:14 +0900

    safe copy

--- kivy_garden/draggable/_utils.py
+++ kivy_garden/draggable/_utils.py
@@ -5,6 +5,7 @@ __all__ = (
 
 from weakref import ref
 from contextlib import contextmanager
+from copy import deepcopy
 
 
 @contextmanager
@@ -37,7 +38,7 @@ def save_widget_location(widget, *, ignore_parent=False) -> dict:
     w = widget.__self__
     location = {
         name: getattr(w, name) for name in _shallow_copyable_property_names}
-    location['pos_hint'] = w.pos_hint.copy()
+    location['pos_hint'] = deepcopy(w.pos_hint)
     if ignore_parent:
         return location
     parent = w.parent
@@ -53,7 +54,7 @@ def restore_widget_location(widget, location: dict, *, ignore_parent=False):
     index = location.get('index', None)
     for name in _shallow_copyable_property_names:
         setattr(w, name, location[name])
-    w.pos_hint = location['pos_hint'].copy()
+    w.pos_hint = deepcopy(location['pos_hint'])
 
     if ignore_parent or weak_parent is None:
         return
