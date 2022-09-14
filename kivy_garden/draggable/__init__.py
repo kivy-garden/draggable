c532f791f3dfd8798c8d74e12c0bbabc270554d1
Nattōsai Mitō <flow4re2c@gmail.com>
2022-09-14 13:11:38 +0900

    remove setup.py

--- kivy_garden/draggable/__init__.py
+++ kivy_garden/draggable/__init__.py
@@ -3,6 +3,5 @@ __all__ = (
     'save_widget_location', 'restore_widget_location', 'ongoing_drags',
 )
 
-from ._version import __version__
 from ._impl import KXDraggableBehavior, KXDroppableBehavior, KXReorderableBehavior, ongoing_drags
 from ._utils import save_widget_location, restore_widget_location
